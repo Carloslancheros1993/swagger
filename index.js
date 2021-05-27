94fcc4a2432ac52d9a3e70f7dcb99278444efcde
carlos <u2204240@unimilitar.edu.co>
2021-05-26 21:33:06 -0500

    login y signup

--- index.js
+++ index.js
@@ -6,6 +6,23 @@ const app = express();
 
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
+app.post("/login", (req, res) => {
+    res.json({
+        "message": "Has iniciado sesion correctamente"
+    })
+});
+
+app.post("/signup", (req, res) => {
+    console.log(req.headers);
+    res.status(201).json({
+        id: 1, 
+        firstName: "Hector",
+        lastName: "Alvarez",
+        email: "hector@gmail.com",
+        password: "$7dsfs89"
+    });
+});
+
 app.listen(9000, () => {
     console.log("Escuchando sobre el puerto 9000");
 });
